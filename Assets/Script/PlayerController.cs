19b71aedcf524868d68dc48575c70f4a05157ec5
WaruGameDev <60048774+WaruGameDev@users.noreply.github.com>
2023-10-02 09:38:12 -0300

    coyote time

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -15,24 +15,79 @@ public class PlayerController : MonoBehaviour
     public float lowMultiplierJump = 2;
     public float fallMultiplierJump = 2.5f;
     public bool canMove = true;
+    public float preBuffTime = .2f;
+    private float actualPreBuff;
+    public bool prebuff;
+    public bool jumping;
+    public bool coyoteJump;
+    public float coyoteTime = .2f;
+    public float actualCoyoteTime;
 
-// Update is called once per frame
+    private void Start()
+    {
+        actualPreBuff = preBuffTime;
+        actualCoyoteTime = coyoteTime;
+    }
+
+    // Update is called once per frame
     void Update()
     {
         isGround = IsGrounded(groundCheckCenter)
                    || IsGrounded(groundCheckLeft) 
                    || IsGrounded(groundCheckRight);
+
+        if (isGround)
+        {
+            jumping = false;
+            coyoteJump = false;
+        }
+
+        if (actualCoyoteTime < coyoteTime)
+        {
+            coyoteJump = true;
+            actualCoyoteTime += 1 * Time.deltaTime;
+            if (actualCoyoteTime >= coyoteTime)
+            {
+                jumping = true;
+                coyoteJump = false;
+            }
+        }
+
+        if (actualPreBuff < preBuffTime)
+        {
+            actualPreBuff += 1 * Time.deltaTime;
+            prebuff = true;
+            if (actualPreBuff >= preBuffTime)
+            {
+                prebuff = false;
+            }
+        }
+
+        if (rb.velocity.y < 0 && !jumping)
+        {
+            actualCoyoteTime = 0;
+        }
+
+        if (jumping)
+        {
+            actualCoyoteTime = coyoteTime;
+        }
+
         if (canMove)
         {
             float x = Input.GetAxis("Horizontal");
             rb.velocity = new Vector2(x * speedPlayer, rb.velocity.y);
-            if (Input.GetButtonDown("Jump") && isGround)
+            if (Input.GetButtonDown("Jump"))
             {
-                rb.velocity = new Vector2(rb.velocity.x, 0);
-                rb.velocity += Vector2.up * jumpPlayer;
+                actualPreBuff = 0;
+            }
+
+            if ((prebuff && isGround) || coyoteJump && prebuff)
+            {
+                Jump();
             }
         }
-        
+       
 
         if (rb.velocity.y < 0)
         {
@@ -42,6 +97,13 @@ public class PlayerController : MonoBehaviour
         {
             rb.velocity += Vector2.up * (Physics2D.gravity.y * ((lowMultiplierJump-1) * Time.deltaTime));
         }
+    }
+
+    public void Jump()
+    {
+        rb.velocity = new Vector2(rb.velocity.x, 0);
+        rb.velocity += Vector2.up * jumpPlayer;
+        jumping = true;
     }
     bool IsGrounded(Transform check)
     {
