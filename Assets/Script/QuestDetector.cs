ec77c1eaf0ad8395042d9ad98a4ee9d2d0a02cb7
kuro9822 <101219351+kuro9822@users.noreply.github.com>
2023-10-30 00:58:11 -0300

    g

--- Assets/Script/QuestDetector.cs
+++ Assets/Script/QuestDetector.cs
@@ -1,5 +1,4 @@
-using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
@@ -8,13 +7,21 @@ public class QuestDetector : MonoBehaviour
 {
     public Quest questToDetect;
     public UnityEvent onQuestDetected;
+    public Collider2D collectableObjectCollider; // Referencia al collider del objeto colectable
 
     public void Detect()
     {
-        if (QuestManager.instance.GetQuestStatus(questToDetect.nameQuest)
-            == QUEST_STATUS.ASSIGNED)
+        if (questToDetect.questStatus == QUEST_STATUS.ASSIGNED)
         {
-            onQuestDetected?.Invoke();
+            // Verifica si el jugador ha tocado el objeto colectable
+            if (collectableObjectCollider != null && collectableObjectCollider.enabled)
+            {
+                // Marca la misión como completa
+                questToDetect.questStatus = QUEST_STATUS.COMPLETE;
+
+                // Invoca el evento de misión completada
+                onQuestDetected?.Invoke();
+            }
         }
     }
 
